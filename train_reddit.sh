5e6dc9443829e7c49988c759beb8537506be170d
w11wo <wilsonwong961@gmail.com>
2023-12-22 12:52:52 +0000

    Added Supervised Fine-tuning Scripts

---
+++ train_reddit.sh
@@ -0,0 +1,6 @@
+sh recipes/indobert_indonlu_base_reddit.sh
+sh recipes/indobert_indonlu_large_reddit.sh
+sh recipes/indobert_indolem_base_reddit.sh
+sh recipes/mbert_base_reddit.sh
+sh recipes/xlmr_base_reddit.sh
+sh recipes/xlmr_large_reddit.sh
