8886b95b88c815d25a8847ee6a244c9b4e49949e
w11wo <wilsonwong961@gmail.com>
2024-01-09 15:00:14 +0000

    Added Twitter Recipes

---
+++ train_twitter.sh
@@ -0,0 +1,7 @@
+# baseline
+sh recipes/twitter/baseline/indobert_indonlu_base_twitter.sh
+sh recipes/twitter/baseline/indobert_indonlu_large_twitter.sh
+sh recipes/twitter/baseline/indobert_indolem_base_twitter.sh
+sh recipes/twitter/baseline/mbert_base_twitter.sh
+sh recipes/twitter/baseline/xlmr_base_twitter.sh
+sh recipes/twitter/baseline/xlmr_large_twitter.sh
